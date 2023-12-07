1d8dfa653aab47e519a710271a95ed8a24321ddd
cipher <w0436300@nscc.ca>
2023-12-06 21:52:24 -0400

    mediatype / theme endpoint

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const db = Database(process.cwd() + '/database/chinook.sqlite');
 const artistsRouter = require('./routes/artists') //will look for index.js
 const albumsRouter = require('./routes/albums') 
 const tracksRouter = require('./routes/tracks') 
+const mediaTypesRouter = require('./routes/mediaTypes') 
+const themesRouter = require('./routes/themes') 
 
 //create an instance of an express app
 const app = express();
@@ -24,25 +26,11 @@ app.use(express.json())//capture data in a post and assign to req.body
 app.use('/api/artists', artistsRouter)
 app.use('/api/albums', albumsRouter)
 app.use('/api/tracks', tracksRouter)
-
-
-//GET all MediaType
-app.get('/api/mediatypes', (req, res) => {
-    const statement = db.prepare('SELECT * FROM media_types')
-    const MediaType = statement.all();
-
-    res.json(MediaType)
-});
+app.use('/api/mediatypes', mediaTypesRouter)
+app.use('/api/themes', themesRouter)
 
 
 
-//THEME
-app.get('/api/themes', (req, res) => {
-    const statement = db.prepare('SELECT * FROM themes')
-    const themes = statement.all();
-
-    res.json(themes)
-});
 
 
 //Listen on the port number, start the service. After successful start-up (nodemon), execute the callback function.
